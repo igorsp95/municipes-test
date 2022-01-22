ec27aa6228dae4aa3720a27671fb0a59883ba5ef
igorparente <igorscheidegger@hotmail.com>
2022-01-21 21:34:42 -0300

    formatting numbers

--- app/controllers/cidadaos_controller.rb
+++ app/controllers/cidadaos_controller.rb
@@ -7,6 +7,8 @@ class CidadaosController < ApplicationController
 
   def show
     @endereco = @cidadao.endereco
+    @cpf = @cidadao.cpf
+    @cpf_formated = formatar_cpf(@cpf)
   end
 
   def new
@@ -33,6 +35,12 @@ class CidadaosController < ApplicationController
   end
 
   private
+  
+  def formatar_cpf(cpf)
+    # cpf = @cidadao.cpf
+    cpf_format = cpf.split('')
+    new_cpf = "#{cpf_format[0]}#{cpf_format[1]}#{cpf_format[2]}.#{cpf_format[3]}#{cpf_format[4]}#{cpf_format[5]}.#{cpf_format[6]}#{cpf_format[7]}#{cpf_format[8]}-#{cpf_format[9]}#{cpf_format[10]}"
+  end
 
   def set_cidadao
     @cidadao = Cidadao.find(params[:id])
--- app/models/cidadao.rb
+++ app/models/cidadao.rb
@@ -2,4 +2,6 @@ class Cidadao < ApplicationRecord
   has_one :endereco, dependent: :destroy
   accepts_nested_attributes_for :endereco
   has_one_attached :photo
+
+  validates :cpf, length: { is: 11 }
 end
