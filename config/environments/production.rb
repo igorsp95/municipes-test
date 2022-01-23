3a5c6558ff09c77d8a6d8d74a60eef8da55d7f27
igorparente <igorscheidegger@hotmail.com>
2022-01-23 16:10:22 -0300

    development n production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,16 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'igorscheidegger.com',
+    user_name:            ENV['user_name'],
+    password:             ENV['mail_password'],
+    authentication:       'plain',
+    }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
