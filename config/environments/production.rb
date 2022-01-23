d6d516a04014aff76ada1a41b1a92c032fbc8588
igorparente <igorscheidegger@hotmail.com>
2022-01-23 16:16:51 -0300

    production url options set

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+  config.action_mailer.default_url_options = { host: "https://municipes-app-igor-parente.herokuapp.com/" }
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
