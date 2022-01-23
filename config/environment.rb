138fc405667c568f5c835a8ac8c0ff5680db361f
igorparente <igorscheidegger@hotmail.com>
2022-01-23 17:48:08 -0300

    heroku error 4

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,5 @@
 # Load the Rails application.
 require_relative 'application'
 
-begin
-  require 'minitest/autorun'
-rescue LoadError => e
-  raise e unless ENV['RAILS_ENV'] == "production"
-end
 # Initialize the Rails application.
 Rails.application.initialize!
