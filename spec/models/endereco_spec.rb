3450bf0b0bca7602eb230c8b3da13ff8784e05bb
igorparente <igorscheidegger@hotmail.com>
2022-01-24 19:16:22 -0300

    test endereco running

--- spec/models/endereco_spec.rb
+++ spec/models/endereco_spec.rb
@@ -3,40 +3,170 @@ require 'rails_helper'
 RSpec.describe Endereco, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
 
-  # cidadao = Cidadao.create!(
-  #   nome: 'Igor',
-  #   sobrenome: "Parente",
-  #   cpf: '11111111116',
-  #   cns: '111111111111116',
-  #   email: "exemplo6@hotmail.com",
-  #   data_nascimento: Time.now,
-  #   telefone: "2121212126",
-  #   status: true)
-
   context 'Criar endereco' do
     it 'com atributos corretos' do   #
-      endereco = Endereco.new(
-        cep: "22793050",
-        logradouro: "Av exemplo",
-        complemento: "300",
-        bairro: "exemplo",
-        cidade: "exemplo",
-        uf: "RJ",
-        codigo_ibge: 1220,
-        cidadao_id: 1
-        ) 
-        expect(endereco).to be_valid  # test code
+      cidadao = Cidadao.new(
+      id: 1,
+      nome: 'Igor',
+      sobrenome: "Parente",
+      cpf: '11111111116',
+      cns: '111111111111116',
+      email: "exemplo6@hotmail.com",
+      data_nascimento: Time.now,
+      telefone: "2121212126",
+      status: true,
+      endereco: Endereco.new(
+      cep: "22793050",
+      logradouro: "Av exemplo",
+      complemento: "300",
+      bairro: "exemplo",
+      cidade: "exemplo",
+      uf: "RJ",
+      codigo_ibge: 1220,
+      cidadao_id: 1
+        )
+      )
+      expect(cidadao.endereco).to be_valid  # test code
     end
   end
 
   context 'Não pode endereços' do  # (almost) plain English
-    it 'sem passar os valores' do   #
+    it 'sem passar os atributos' do   #
       expect { Endereco.create! }.to raise_error(ActiveRecord::RecordInvalid)  # test code
     end
 
     it 'sem passar cep' do   #
-      expect { Endereco.create! }.to raise_error(ActiveRecord::RecordInvalid)  # test code
+      cidadao = Cidadao.new(
+      id: 1,
+      nome: 'Igor',
+      sobrenome: "Parente",
+      cpf: '11111111116',
+      cns: '111111111111116',
+      email: "exemplo6@hotmail.com",
+      data_nascimento: Time.now,
+      telefone: "2121212126",
+      status: true,
+      endereco: Endereco.new(
+      logradouro: "Av exemplo",
+      complemento: "300",
+      bairro: "exemplo",
+      cidade: "exemplo",
+      uf: "RJ",
+      codigo_ibge: 1220,
+      cidadao_id: 1
+        )
+      )
+      expect(cidadao.endereco).not_to be_valid  # test code
+    end
+
+    it 'sem passar logradouro' do   #
+      cidadao = Cidadao.new(
+      id: 1,
+      nome: 'Igor',
+      sobrenome: "Parente",
+      cpf: '11111111116',
+      cns: '111111111111116',
+      email: "exemplo6@hotmail.com",
+      data_nascimento: Time.now,
+      telefone: "2121212126",
+      status: true,
+      endereco: Endereco.new(
+      cep: "22793050",
+      complemento: "300",
+      bairro: "exemplo",
+      cidade: "exemplo",
+      uf: "RJ",
+      codigo_ibge: 1220,
+      cidadao_id: 1
+        )
+      )
+      expect(cidadao.endereco).not_to be_valid  # test code
+    end
+
+    it 'sem passar cidade' do   #
+      cidadao = Cidadao.new(
+      id: 1,
+      nome: 'Igor',
+      sobrenome: "Parente",
+      cpf: '11111111116',
+      cns: '111111111111116',
+      email: "exemplo6@hotmail.com",
+      data_nascimento: Time.now,
+      telefone: "2121212126",
+      status: true,
+      endereco: Endereco.new(
+      cep: "22793050",
+      logradouro: "Av exemplo",
+      complemento: "300",
+      bairro: "exemplo",
+      uf: "RJ",
+      codigo_ibge: 1220,
+      cidadao_id: 1
+        )
+      )
+      expect(cidadao.endereco).not_to be_valid  # test code
+    end
+
+    it 'sem passar bairro' do   #
+      cidadao = Cidadao.new(
+      id: 1,
+      nome: 'Igor',
+      sobrenome: "Parente",
+      cpf: '11111111116',
+      cns: '111111111111116',
+      email: "exemplo6@hotmail.com",
+      data_nascimento: Time.now,
+      telefone: "2121212126",
+      status: true,
+      endereco: Endereco.new(
+      cep: "22793050",
+      logradouro: "Av exemplo",
+      complemento: "300",
+      cidade: "exemplo",
+      uf: "RJ",
+      codigo_ibge: 1220,
+      cidadao_id: 1
+        )
+      )
+      expect(cidadao.endereco).not_to be_valid  # test code
+    end
+
+    it 'sem passar uf' do   #
+      cidadao = Cidadao.new(
+      id: 1,
+      nome: 'Igor',
+      sobrenome: "Parente",
+      cpf: '11111111116',
+      cns: '111111111111116',
+      email: "exemplo6@hotmail.com",
+      data_nascimento: Time.now,
+      telefone: "2121212126",
+      status: true,
+      endereco: Endereco.new(
+      cep: "22793050",
+      logradouro: "Av exemplo",
+      complemento: "300",
+      bairro: "exemplo",
+      cidade: "exemplo",
+      codigo_ibge: 1220,
+      cidadao_id: 1
+        )
+      )
+      expect(cidadao.endereco).not_to be_valid  # test code
+    end
+
+    it 'sem cidadao' do   #
+      endereco = Endereco.new(
+      cep: "22793050",
+      logradouro: "Av exemplo",
+      complemento: "300",
+      bairro: "exemplo",
+      cidade: "exemplo",
+      uf: "RJ",
+      codigo_ibge: 1220,
+      cidadao_id: 1
+      )
+      expect(endereco).not_to be_valid  # test code
     end
   end
-
 end
